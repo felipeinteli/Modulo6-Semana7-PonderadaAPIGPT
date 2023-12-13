61f112fd619e9cc9b29a33d03f7e88e663d0e620
felipeinteli <felipe.moura@sou.inteli.edu.br>
2023-12-13 07:24:24 -0300

    Subindo código completo

--- app/services.py
+++ app/services.py
@@ -1,10 +1,14 @@
 import logging
+import openai
+import requests
 from app import models
 
-
+openai.api_key = 'your-api-key-here'
 class HistoryServices:
 
     def create_history(history_map):
+
+
         try:
             with models.HistoryDAO() as dao:
                 dao.create_history(history_map)
@@ -21,4 +25,16 @@ class HistoryServices:
             return result
         except Exception as e:
             return []
+        
+
+    def chat_gpt_story(prompt):
+
+        response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt="Translate the following English text to French: '{}'",
+        max_tokens=60
+        )
+
+        return response
+
 
